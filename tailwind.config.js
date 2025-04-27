4334184ac02b58844a752e2e840674e3efaded8d
amonta16 <amonta16@calpoly.edu>
2025-04-26 19:12:49 -0700

    Fix Tailwind and PostCSS versions for Vercel build

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,7 @@ module.exports = {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  darkMode: false, // or 'media' or 'class',
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {},
   },
   plugins: [],
